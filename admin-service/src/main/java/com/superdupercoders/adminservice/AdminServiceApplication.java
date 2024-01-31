157d20343b6f10d1342fbfbb6aa5f901707d4fa2
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2024-01-31 23:27:46 +0100

    Add service discovery client REST endpoint for discovering instances of other services

--- admin-service/src/main/java/com/superdupercoders/adminservice/AdminServiceApplication.java
+++ admin-service/src/main/java/com/superdupercoders/adminservice/AdminServiceApplication.java
@@ -2,7 +2,14 @@ package com.superdupercoders.adminservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AdminServiceApplication {
 
