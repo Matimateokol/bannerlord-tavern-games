47cfabfd9982dd4bcba17915ebd4a41a0992bcbe
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2024-02-02 17:29:18 +0100

    Add few unit tests

--- bannerlord-tavern-games-frontend/src/handlers/rollDice.test.js
+++ bannerlord-tavern-games-frontend/src/handlers/rollDice.test.js
@@ -1,4 +1,4 @@
-const rollDice = require('./rollDice');
+import { rollDice } from './rollDice';
 
 test('checks if rolled number is less than or equal to 5', () => {
     expect(rollDice(1, 5)).toBeLessThanOrEqual(5);
@@ -6,4 +6,12 @@ test('checks if rolled number is less than or equal to 5', () => {
 
 test('checks if rolled number is greater than or equal to 1', () => {
     expect(rollDice(1, 5)).toBeGreaterThanOrEqual(1);
+});
+
+test('checks if rolled output is a number', () => {
+    expect(rollDice(1, 5)).not.toBeNaN();
+});
+
+test('checks if rolled output is a NaN', () => {
+    expect(rollDice(NaN, NaN)).toBeNaN();
 });
